8d29b03fac461e75d3f84aa91cc0a0e295fd2cfb
itsunknown01 <aryamangohain@gmail.com>
2023-08-22 17:39:01 +0530

    Create root dIrectories and header
    Create the root directories of home and about page. ALso created the header layout and styled with tailwindcss

---
+++ src/data/constant.js
@@ -0,0 +1,26 @@
+import educators from "../assests/images/Faculty.jpeg"
+import Student from "../assests/images/Student.jpeg"
+import events from "../assests/images/events.jpeg"
+
+export const NavbarItems = ["Home", "About", "Course", "Blog", "Contact"];
+
+export const AboutSectionData = [
+  {
+    title: "Dedicated Educators",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet nulla facilisi morbi tempus iaculis. Sed pulvinar proin gravida hendrerit. Pulvinar pellentesque habitant morb tristique senectus.",
+    image: educators,
+  },
+  {
+    title: "First-Rate Curriculum",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo incididunt ut labore et dolore magna aliqua. Sit amet nulla facilisi morbi tempus iaculis. Sed pulvinar proin gravida hendrerit. Pulvinar pellentesque habitant morb tristique senectus.",
+    image: Student,
+  },
+  {
+    title: "Exciting Extracurriculars",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo incididunt ut labore et dolore magna aliqua. Sit amet nulla facilisi morbi tempus iaculis. Sed pulvinar proin gravida hendrerit. Pulvinar pellentesque habitant morb tristique senectus.",
+    image: events,
+  },
+];
---
+++ src/layout/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import logo from "../assests/images/logo.png";
+import { NavbarItems } from "../data/constant";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const handleClick = (menuitem) => {
+    if (menuitem === "Home") {
+      navigate("/");
+    } else {
+      navigate(`/${menuitem.toLowerCase()}`);
+    }
+  };
+  return (
+    <header className="bg-white fixed shadow-xl w-full">
+      <nav
+        className="flex items-center justify-between p-4 lg:px-6 my-0 mx-10"
+        aria-label="Global"
+      >
+        <a href="/" className="-m-1.5 p-1.5">
+          <img src={logo} alt="logo" className="w-40 object-contain bg-black" />
+        </a>
+        <ul className="relative py-2 flex list-none">
+          {NavbarItems.map((item, index) => (
+            <li
+              key={index}
+              onClick={()=> handleClick(item)}
+              className="py-1.5 px-4 box-border cursor-pointer whitespace-nowrap align-middle relative justify-start"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
